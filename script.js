6f894e644f9e38be5ddab11c3cf480e8971935d4
Amandeep715 <89999002+Amandeep715@users.noreply.github.com>
2021-10-12 18:09:28 +0200

    initial-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+    question.addEventListener("click", () => {
+        if(!question.nextElementSibling.classList.contains("open")) {
+            const checkOtherItems = document.querySelectorAll(".open")
+            checkOtherItems.forEach((otherItem) => {
+                otherItem.previousElementSibling.classList.remove("active")
+                otherItem.classList.remove("open");
+            })
+            question.classList.add("active");
+            question.nextElementSibling.classList.add("open");
+        } else {
+            question.classList.remove("active");
+            question.nextElementSibling.classList.remove("open");
+        }
+    })
+})
