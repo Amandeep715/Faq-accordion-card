d1a0a7fa792590882733559a62a994165073844e
Amandeep715 <89999002+Amandeep715@users.noreply.github.com>
2021-10-14 16:46:43 +0200

    Use-setAttribute-to-add-aria-expanded

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ questions.forEach((question) => {
             checkOtherItems.forEach((otherItem) => {
                 otherItem.previousElementSibling.classList.remove("active")
                 otherItem.classList.remove("open");
+                otherItem.previousElementSibling.setAttribute("aria-expanded", "false");
             })
             question.classList.add("active");
             question.nextElementSibling.classList.add("open");
+            question.setAttribute("aria-expanded", "true");
         } else {
             question.classList.remove("active");
             question.nextElementSibling.classList.remove("open");
+            question.setAttribute("aria-expanded", "false");
         }
     })
 })
